0d07935ed9baf839c03b67ebe4ff78b5c9d650a6
marc <marcmayolorell@gmail.com>
2020-04-12 08:46:58 +0200

    listado publico de animales

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {fadeIn} from '../animations';
+import {AnimalService} from '../services/animal.service';
+import {Animal} from '../models/animal.model';
+import {GLOBAL} from '../services/globals';
+
 @Component({
   selector: 'app-animals',
   templateUrl: './animals.component.html',
   styleUrls: ['./animals.component.css'],
+  providers: [AnimalService],
   animations: [fadeIn]
 })
 export class AnimalsComponent implements OnInit {
-  title = 'Animales';
+  public title: string;
+  public animals: Animal[];
+  public url: string;
 
-  constructor() {
+  constructor(
+    private animalService: AnimalService
+  ) {
+    this.title = 'Animales';
+    this.url = GLOBAL.url;
   }
 
+
   ngOnInit(): void {
-    console.log('animals.component cargado !!');
+    this.getAnimals();
+  }
+
+  getAnimals() {
+    this.animalService.getAnimals().subscribe(res => {
+      // @ts-ignore
+      if (res.animals) {
+        // @ts-ignore
+        this.animals = res.animals;
+      }
+    });
   }
 
 }
